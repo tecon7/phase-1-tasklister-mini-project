dde8edfc540ec41fad4141b2ea92b9720946f996
Teddy Economou <teddyecon@gmail.com>
2023-01-12 16:57:29 -0500

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskForm = document.querySelector('#create-task-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    const descriptionField = document.querySelector('#new-task-description')
+
+    const taskList = document.querySelector('#tasks')
+    const newTask = document.createElement('li')
+    newTask.textContent = descriptionField.value
+
+    const priority = document.querySelector('#priority')
+
+    if (priority.value === 'High') {
+      newTask.classList.add('red')
+    } else if (priority.value === 'Medium') {
+      newTask.classList.add('yellow')
+    } else {
+      newTask.classList.add('green')
+    }
+
+    newTask.classList.add(priority.value)
+    
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'X'
+    deleteButton.addEventListener('click', (event) => {
+      newTask.remove()
+    })
+    newTask.append(deleteButton)
+    taskList.append(newTask)
+
+
+  })
+
+})
